8e24fcbd5f3087cba60e23ab52e0075ce4da7115
Chris Yocum <cyocum@gmail.com>
2010-11-29 14:16:08 +0000

    working code

--- bessel.ml
+++ bessel.ml
@@ -1,25 +1,24 @@
-module Bessel = struct
-  let factorial n =
-    let rec f n a =
-      match n with
-        | 0 -> a
-        | _ -> f (n-1) (n*a)
-    in
+let factorial n =
+  let rec f n a =
+    match n with
+      | 0 -> a
+      | _ -> f (n-1) (n*a)
+  in
     f n 1
   
-  let mod_bessel_first m a =
-    (1. /. ((float (factorial m)) *. (Lanczos.gamma (float (m + a + 1)))))
+let mod_bessel_first m a =
+  (1. /. ((float (factorial m)) *. (Lanczos.gamma (float (m + a + 1)))))
 
-  let mod_bessel_second m x a =
-    x /. 2. ** (float (2 * m + a))
+let mod_bessel_second m x a =
+  x /. 2. ** (float (2 * m + a))
 
-  let mod_bessel m a x =
-    (mod_bessel_first m a) *. (mod_bessel_second m x a)
- 
-  let run_mod_bessel a x =
-    let ans_list = BatList.map (fun m -> mod_bessel m a x) (BatEnum.range 0 1000) in
-      List.fold_left (+.) 0 ans_list
+let mod_bessel m a x =
+  (mod_bessel_first m a) *. (mod_bessel_second m x a)
 
-end
+let run_mod_bessel a x =
+  let range = (BatList.of_enum (BatEnum.range 0 ~until:50)) in
+  let ans_list = BatList.map (fun m -> mod_bessel m a x) range in
+    (*List.iter (fun x -> print_endline (string_of_float x)) ans_list;*)
+    List.fold_left (+.) 0. ans_list
 
 
--- skellam.ml
+++ skellam.ml
@@ -1,4 +1,10 @@
 let skellam k u1 u2 =
-    (Lanczos.e exp -(u1 +. u2)) * ((u1 /. u2) exp (k / 2)) * (Bessel.mod_bessel (abs_float k)
-    (2 *. sqrt (u1 +. u2)))  
+  let first = Lanczos.e ** -.(u1 +. u2) in
+  let second = (u1 /. u2) ** ((float k) /. 2.) in
+  let third = 2. *. sqrt (u1 +. u2) in
+    first *. second *. (Bessel.run_mod_bessel (abs k) third)
 
+let _ =
+  let blah = -1 in 
+  let output = skellam blah 1.5 2.0 in
+    print_endline (string_of_float output)
