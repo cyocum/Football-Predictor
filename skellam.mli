d560a890df22b45a3ce36895a7b2c6460a209795
Chris Yocum <cyocum@gmail.com>
2010-12-04 15:20:45 +0000

    added interface file for skellam

---
+++ skellam.mli
@@ -0,0 +1,4 @@
+val skellam : int -> float -> float -> float
+val cartesian_product : 'a list -> 'b list -> ('a * 'b) list
+val run_prediction : (int * int) -> float
+val print_predictions : float -> int * int -> unit
