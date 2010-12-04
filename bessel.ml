f7c2b3018bf9d6d4f820894d3014c586a0cb8bb4
Chris Yocum <cyocum@gmail.com>
2010-12-04 13:54:44 +0000

    removed two functions for lexical bindings

--- bessel.ml
+++ bessel.ml
@@ -6,15 +6,12 @@ let factorial n =
   in
     fact_aux n 1
   
-let mod_bessel_first m a =
-  (1. /. ((float (factorial m)) *. (Lanczos.gamma (float (m + a + 1)))))
-
-let mod_bessel_second m x a =
-  (x /. 2.) ** (float (2 * m + a))
-
 let mod_bessel m a x =
-  (mod_bessel_first m a) *. (mod_bessel_second m x a)
-
+  let first = (1. /. ((float (factorial m)) *. (Lanczos.gamma (float (m + a +
+  1))))) in
+  let second = (x /. 2.) ** (float (2 * m + a)) in
+    first *. second
+  
 let run_mod_bessel a x =
   let range = (BatList.of_enum (BatEnum.range 0 ~until:50)) in
   let ans_list = BatList.map (fun m -> mod_bessel m a x) range in
