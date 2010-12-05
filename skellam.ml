b8fd75e92f497a46f799feb2597f3eea9fb3e74f
Chris Yocum <cyocum@gmail.com>
2010-12-05 14:03:24 +0000

    fixed major bug in skellam formula

--- skellam.ml
+++ skellam.ml
@@ -4,7 +4,7 @@ let away_team_gpm = (float_of_string Sys.argv.(2))
 let skellam k u1 u2 =
   let first = Lanczos.e ** -.(u1 +. u2) in
   let second = (u1 /. u2) ** ((float k) /. 2.) in
-  let third = 2. *. sqrt (u1 +. u2) in
+  let third = 2. *. sqrt (u1 *. u2) in
     first *. second *. (Bessel.run_mod_bessel (abs k) third)
 
 let cartesian_product xs ys =
