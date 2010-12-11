4074ca1e48536bd0c3d05c3cd3f3f6dbdf40b69f
Chris Yocum <cyocum@gmail.com>
2010-12-11 18:00:10 +0000

    added inference target to makefile and cleaned up the main file so that it uses currying and types to choose the outcomes

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 build:
 	ocamlfind ocamlopt -package batteries -linkpkg -o football lanczos.mli bessel.mli lanczos.ml bessel.ml skellam.mli skellam.ml football.mli football.ml main.ml
 
+infer:
+	ocamlfind ocamlopt -package batteries -linkpkg -o football -i lanczos.mli bessel.mli lanczos.ml bessel.ml skellam.mli skellam.ml football.mli football.ml main.ml
+
 clean:
 	rm *.cm* *.o football 
--- main.ml
+++ main.ml
@@ -1,13 +1,20 @@
+type outcome = Win | Lose | Draw
+
+let calc_outcome outcome =
+  let calc_outcome_aux = BatList.map Football.run_prediction in
+  let fold_float = List.fold_left (+.) 0. in
+   match outcome with
+      | Win -> 
+        fold_float (calc_outcome_aux Football.wins)
+      | Lose ->
+        fold_float (calc_outcome_aux Football.lose)
+      | Draw ->
+        fold_float (calc_outcome_aux Football.draw)
+
 let _ =
-  let wins = Football.wins in
-  let draw = Football.draw in
-  let lose = Football.lose in
-  let wins_percent_lst = BatList.map Football.run_prediction wins in
-  let draw_percent_lst = BatList.map Football.run_prediction draw in
-  let lose_precent_lst = BatList.map Football.run_prediction lose in
-  let win_percent = List.fold_left (+.) 0. wins_percent_lst in
-  let draw_percent = List.fold_left (+.) 0. draw_percent_lst in
-  let lose_percent =  List.fold_left (+.) 0. lose_precent_lst in
+  let win_percent = calc_outcome Win in
+  let draw_percent = calc_outcome Draw in
+  let lose_percent = calc_outcome Lose in
     print_endline ("win --> " ^ (string_of_float win_percent));
     print_endline ("draw --> " ^ (string_of_float draw_percent));
-    print_endline ("lose --> " ^ (string_of_float lose_percent));
+    print_endline ("lose --> " ^ (string_of_float lose_percent));   
