b0980828e76bf3c4b332875331579a2e70b491ce
Chris Yocum <cyocum@gmail.com>
2012-03-25 14:18:43 +0100

    removed useless lamba in favor of currying and took out subtract for pred

--- bessel.ml
+++ bessel.ml
@@ -2,11 +2,11 @@ let factorial n =
   let rec fact_aux n a =
     match n with
       | 0 -> a
-      | _ -> fact_aux (n-1) (n*a)
+      | _ -> fact_aux (pred n) (n*a)
   in
     fact_aux n 1
   
-let mod_bessel m a x =
+let mod_bessel a x m =
   let first = (1. /. ((float (factorial m)) *. (Lanczos.gamma (float (m + a +
   1))))) in
   let second = (x /. 2.) ** (float (2 * m + a)) in
@@ -14,5 +14,6 @@ let mod_bessel m a x =
   
 let run_mod_bessel a x =
   let range = (BatList.of_enum (BatEnum.range 0 ~until:50)) in
-  let ans_list = BatList.map (fun m -> mod_bessel m a x) range in
+  let mb = mod_bessel a x in
+  let ans_list = BatList.map mb range in
     List.fold_left (+.) 0. ans_list
